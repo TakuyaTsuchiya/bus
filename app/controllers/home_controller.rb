90ac4470bd91862e3bcb8e922ec3350aaecf3021
Takuya Tsuchiya <takuya21@gmail.com>
2021-07-26 16:03:06 +0900

    start bus stop and end bus stop implement done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,25 @@
 class HomeController < ApplicationController
-  def show
-    @current_bus_stop = BusStop.find(1)
-    @target_bus_stop = BusStop.find(4)
+  def show(start_bus_stop_id: nil, end_bus_stop_id: nil)
+    if start_bus_stop_id
+      @start_bus_stop = BusStop.find(start_bus_stop_id)
+    end
 
-    candidate_orders = BusStopOrder.where(bus_stop: @target_bus_stop)
-    candidate_bus_flows = BusFlow.where(bus_stop_orders: candidate_orders)
-    target_orders = BusStopOrder.where(bus_flow: candidate_bus_flows, bus_stop: @current_bus_stop)
-    @target_order = target_orders.where("arrived_time >= ?", Time.zone.now).take
-    @target_order ||= target_orders.order(arrived_time: :asc).take
+    if end_bus_stop_id
+      @end_bus_stop = BusStop.find(end_bus_stop_id)
+    end
+
+    if @start_bus_stop && @end_bus_stop
+      candidate_orders = BusStopOrder.where(bus_stop: @end_bus_stop)
+      candidate_bus_flows = BusFlow.where(bus_stop_orders: candidate_orders)
+      target_orders = BusStopOrder.where(bus_flow: candidate_bus_flows, bus_stop: @start_bus_stop)
+      @target_order = target_orders.where("arrived_time >= ?", Time.zone.now).take
+      @target_order ||= target_orders.order(arrived_time: :asc).take
+    end
 
     @bus_stops = BusStop.all
     @bus_stop_orders = BusStopOrder.all
     @bus_flows = BusFlow.all
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "home#show"
+  get  "/getbus", to: "home#aaa"
 end
